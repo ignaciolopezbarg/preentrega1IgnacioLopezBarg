73ea139c688c8924750570e0210149f634c44483
ignacio lopez barg <ignaciolopezbar@hotmail.com>
2024-04-15 19:02:11 -0300

    rutas

--- src/pages/Ofertas.jsx
+++ src/pages/Ofertas.jsx
@@ -7,7 +7,7 @@ function Ofertas() {
     fetch("/imagenes.json/category/sports")
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Error al cargar los datos");
+          throw new Error("Error al cargar las ofertas");
         }
         return response.json();
       })
@@ -15,15 +15,17 @@ function Ofertas() {
         setImages(data);
       })
       .catch((error) => {
-        console.error("Error busqueda de los datos:", error);
+        console.error("Error busqueda de ofertas:", error);
       });
   }, []);
 
   return (
     <div className="flex flex-wrap ml-10 max-w-1200 mx-auto ">
-     
       {images.map((image) => (
-        <div key={image.id} className="m-2 flex flex-col items-center border-2 solid rounded-xl">
+        <div
+          key={image.id}
+          className="m-2 flex flex-col items-center border-2 solid rounded-xl"
+        >
           <img
             src={`/img/${image.img}`}
             alt={image.name}
@@ -31,6 +33,7 @@ function Ofertas() {
           />
           <div className="mt-2  gap-2 flex flex-col items-center md:flex-row md:justify-between">
             <p className="text-xxs font-bold">{image.name}</p>
+            <span> {image.price} </span>
             <button className="bg-slate-400 w-14 h-6 rounded-md font-serif text-sm text-white">
               comprar
             </button>
@@ -40,6 +43,5 @@ function Ofertas() {
     </div>
   );
 }
-
 
 export default Ofertas;
