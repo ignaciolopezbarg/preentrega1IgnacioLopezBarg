638f8ed545af4432c2f250dfd0dcc73f77316cd4
ignacio lopez barg <ignaciolopezbar@hotmail.com>
2024-04-14 21:35:07 -0300

    se restauro la navbar

--- src/components/FunkosMenu.jsx
+++ src/components/FunkosMenu.jsx
@@ -1,38 +1,38 @@
-// import { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import { ChevronDown } from 'lucide-react';
+ import { useState } from 'react';
+ import { Link } from 'react-router-dom';
+ import { ChevronDown } from 'lucide-react';
 
-// function FunkosMenu() {
-//   const [isOpen, setIsOpen] = useState(false);
+ function FunkosMenu() {
+  const [isOpen, setIsOpen] = useState(false);
 
-//   const subMenu = () => {
-//     setIsOpen(!isOpen);
-//   };
+  const subMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-//   return (
-//     <div className="relative">
-//       <button
-//         onClick={subMenu}
-//         className="hover:bg-teal-300 rounded-md px-2 py-1 flex items-center focus:outline-none "
-//       >
-//         <span className="">Funkos</span>
-//          <ChevronDown  />
-//       </button>
-//       {isOpen &&  (
-//         <div className=" absolute top-full left-0  bg-black shadow-lg rounded-md p-2">
-//           <Link to="/funkos/harry_potter" className="block hover:text-blue-500">
-//            <span className='submenu__title'> harry_potter</span>
-//           </Link>
-//           <Link to="/funkos/pokemon" className="block hover:text-blue-500">
-//             <span className='submenu__title'>pokemon</span>
-//           </Link>
-//           <Link to="/funkos/stars_wars" className="block hover:text-blue-500">
-//            <span className='submenu__title'>stars_wars</span> 
-//           </Link>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
+   return (
+   <div className="relative">
+       <button
+        onClick={subMenu}
+        className="hover:bg-teal-300 rounded-md px-2 py-1 flex items-center focus:outline-none "
+      >
+        <span className="">Funkos</span>
+        <ChevronDown  />
+      </button>
+       {isOpen &&  (
+        <div className=" absolute top-full left-0  bg-black shadow-lg rounded-md p-2">
+         <Link to="/funkos/harry_potter" className="block hover:text-blue-500">
+          <span className='submenu__title'> harry_potter</span>
+          </Link>
+          <Link to="/funkos/pokemon" className="block hover:text-blue-500">
+            <span className='submenu__title'>pokemon</span>
+          </Link>
+           <Link to="/funkos/stars_wars" className="block hover:text-blue-500">
+           <span className='submenu__title'>stars_wars</span> 
+         </Link>
+        </div>
+      )}
+   </div>
+  )
+ }
 
-// export default FunkosMenu;
+ export default FunkosMenu;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import Logo from "../img/funkos-banner.webp";
 import { Link } from "react-router-dom";
 import { MenuIcon } from "lucide-react";
+import FunkosMenu from "./FunkosMenu";
 import Cart_Item from "./Cart_Item";
 
 function Navbar() {
@@ -16,13 +17,8 @@ function Navbar() {
         </Link>
       </div>
 
-      <div className="flex md:gap-8">
-        <Link
-          to="/muñecos"
-          className="hover:bg-teal-300 hidden md:flex rounded-md"
-        >
-          Funkos
-        </Link>
+      <div className="hidden md:flex gap-8">
+    <FunkosMenu />
         <Link
           to="/ofertas"
           className="hover:bg-teal-300 hidden md:flex rounded-md"
@@ -43,107 +39,4 @@ function Navbar() {
     </nav>
   );
 }
-export default Navbar;
-
-// import React from "react";
-// import FunkosMenu from "./FunkosMenu.jsx";
-// import Logo from "../img/funkos-banner.webp";
-// import { Link } from "react-router-dom";
-// import { MenuIcon } from "lucide-react";
-// import Cart_Item from "./Cart_Item";
-
-// function Navbar() {
-//   return (
-//     <nav className="max-w-3xl mx-auto flex items-center justify-between gap-2 px-4 py-2 md:px-0">
-//       <div className="flex flex-wrap rounded-xl h-20 w-80 p-0 overflow-hidden">
-//         <Link to="/" className="flex p-1 items-center gap-2">
-//           <img
-//             src={Logo}
-//             alt="Imagen de la tienda Funkoshop"
-//             // className="h-10 rounded-xl md:h-auto md:w-48 lg:w-68"
-//           />
-//         </Link>
-//       </div>
-//       <div className="hidden md:flex gap-8">
-//        {/* // <FunkosMenu /> Aquí se renderiza FunkosMenu */}
-//         <Link to="/ofertas" className="hover:bg-teal-300 rounded-md px-2 py-1">
-//           Ofertas
-//         </Link>
-//         <Link to="/contacto" className="hover:bg-teal-300 rounded-md px-2 py-1">
-//           Contacto
-//         </Link>
-//       </div>
-//       <MenuIcon width={20} height={20} className="md:hidden" />
-//       <div className="cart_container">
-//         <Cart_Item />
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default Navbar;
-
-// //  import FunkosMenu from "./FunkosMenu.jsx";
-
-// //   const [isOpen, setIsOpen] = useState(false);
-
-// //   const toggleMenu = () => {
-// //     setIsOpen(!isOpen);
-// //   };
-
- 
-  
-// // import React, { useState } from "react";
-// // import { Link } from "react-router-dom";
-// // import { ChevronDown, Menu as MenuIcon } from "lucide-react";
-// // import Logo from "../img/funkos-banner.webp";
-// // import FunkosMenu from "./FunkosMenu.jsx"; // Importa el componente FunkosMenu
-// // import Contacto from "../pages/Contacto.jsx"; // Importa el componente Contacto
-// // import Ofertas from "../pages/Ofertas.jsx"; // Importa el componente Ofertas
-// // import CartItem from "./Cart_Item.jsx"; // Importa el componente CartItem
-
-// // function Navbar() {
-// //   const [isOpen, setIsOpen] = useState(false);
-
-// //   const toggleMenu = () => {
-// //     setIsOpen(!isOpen);
-// //   };
-
-// //   return (
-// //     <nav className="max-w-3xl mx-auto flex items-center justify-between gap-2 px-4 py-2 md:px-0">
-// //       <Link to="/" className="flex items-center gap-2">
-// //         <img
-// //           src={Logo}
-// //           alt="Imagen de la tienda Funkoshop"
-// //           className="h-10 rounded-xl md:h-auto md:w-48 lg:w-68"
-// //         />
-// //       </Link>
-
-// //       <div className="relative">
-// //         <button
-// //           onClick={toggleMenu}
-// //           className="hover:bg-teal-300 rounded-md px-2 py-1 flex items-center focus:outline-none "
-// //         >
-// //           <span className="">Funkos</span>
-// //           <ChevronDown />
-// //         </button>
-// //         {isOpen && <FunkosMenu />}
-// //       </div>
-
-// //       <Link to="/ofertas" className="hover:bg-teal-300 rounded-md px-2 py-1">
-// //         Ofertas
-// //       </Link>
-// //       <Link to="/contacto" className="hover:bg-teal-300 rounded-md px-2 py-1">
-// //         Contacto
-// //       </Link>
-
-// //       <div className="cart_container">
-// //         <CartItem />
-// //       </div>
-
-// //       <MenuIcon width={20} height={20} className="md:hidden" />
-// //     </nav>
-// //   );
-// // }
-
-// // export default Navbar;
+export default Navbar
