72581beda27218476565d45b0b45027188a018bb
ignacio lopez barg <ignaciolopezbar@hotmail.com>
2024-04-16 09:41:04 -0300

    rutas

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,27 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 import ImageList from "./ImageList";
-import Ofertas from "../pages/Ofertas";
-import Contacto from "../pages/Contacto";
+// import Ofertas from "../pages/Ofertas";
+// import Contacto from "../pages/Contacto";
 import Saludo from "./saludo";
-import ItemListContainer from "./itemListContainer";
+// import ItemListContainer from "./itemListContainer";
 
 function Main() {
   return (
     <>
       <Saludo />
-      <Routes>
-         <Route path="/Ofertas" element={<Ofertas />} /> 
-         <Route path="/Contacto" element={<Contacto />} /> 
-        <Route path="/" element={<ImageList />} />
-        <Route path="/img/:id" element={<itemListContainer />} />
-      </Routes>
-      
+
       <ImageList />
     </>
   );
 }
 
 export default Main;
-
-
- 
