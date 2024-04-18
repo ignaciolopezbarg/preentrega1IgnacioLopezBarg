16c632707770737dd4ccc9ba27cbba8e6c6973b4
ignacio lopez barg <ignaciolopezbar@hotmail.com>
2024-04-18 15:39:46 -0300

    arreglos pagina item

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
@@ -25,33 +24,44 @@ function ItemList() {
   const selectedItem = images.find((image) => image.id === itemId);
 
   if (!selectedItem) {
-    return 
-    <div >
-Esperando carga...
-    </div>; 
+    return;
+
+    <div>Esperando carga...</div>;
   }
 
   return (
-    
-    <div className=" flex flex-col p-5  justify-center  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-auto max-w-8xl">
-      {selectedItem && (
-        <div className="border border-gray-700 bg-slate-300 rounded-lg overflow-hidden shadow-lg">
-          <img src={`/img/${selectedItem.img}`} alt={selectedItem.name} className="w-full h-64 object-cover" />
-          <div className="p-4">
-            <p className=" ml-20 text-lg font-semibold mb-2">{selectedItem.name}</p>
-            <p className="text-gray-700 font-bold text-xl  ml-20"  >{selectedItem.price}</p>
-            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-2 ml-20">
-              Comprar
-            </button>
+    <>
+      <div>
+        <h1 className="item__encabezado p-5 flex justify-center text-3xl font-bold"> BIENVENIDO A LA PAGINA DE COMPRAS </h1>
+      </div>
+
+      <div
+        className=" flex flex-col p-5  justify-center  grid-cols-1 
+     gap-4 mx-auto max-w-8xl"
+      >
+        {selectedItem && (
+          <div className="border border-gray-700 bg-slate-300 rounded-lg overflow-hidden shadow-lg">
+            <img
+              src={`/img/${selectedItem.img}`}
+              alt={selectedItem.name}
+              className="w-full h-64 object-cover"
+            />
+            <div className="p-4">
+              <p className=" ml-20 text-lg font-semibold mb-2">
+                {selectedItem.name}
+              </p>
+              <p className="text-gray-700 font-bold text-xl  ml-20">
+                {selectedItem.price}
+              </p>
+              <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-2 ml-20">
+                Comprar
+              </button>
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 }
 
 export default ItemList;
-
-
-
-
