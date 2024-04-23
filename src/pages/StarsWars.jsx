94df6789d376824f584e2c359be00b4987a7bcfb
ignacio lopez barg <ignaciolopezbar@hotmail.com>
2024-04-23 20:19:54 -0300

    items

--- src/pages/StarsWars.jsx
+++ src/pages/StarsWars.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 function StarsWars() {
+  const { category } = useParams();
+  const [starswarsItems, setStarswarsItems] = useState([]);
+
+  useEffect(() => {
+    fetch('/imagenes.json')
+      .then(response => response.json())
+      .then(data => {
+        const starswarsItems = data.filter(item => item.category === 'starswars');
+        setStarswarsItems(starswarsItems);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
-    <div> <p>StarsWars</p></div>
-  )
+    <div className="grid grid-cols-3 gap-4 p-4">
+      {starswarsItems.map(item => (
+        <div key={item.id} className="bg-gray-200 p-4">
+          <img src={item.img} alt={item.name} className="mx-auto" />
+          <h2 className="text-lg font-bold">{item.name}</h2>
+          <p className="text-gray-700">{item.price}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default StarsWars
+export default StarsWars;
